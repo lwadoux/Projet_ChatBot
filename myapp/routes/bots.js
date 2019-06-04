530a127bc64e7adfdec5342f32e418cae93758b4
Lily <lil.wad.44@gmail.com>
2019-06-04 09:56:32 +0200

    Parameters in the URL

--- myapp/routes/bots.js
+++ myapp/routes/bots.js
@@ -8,16 +8,16 @@ var router = express.Router();
  * @summary Define the get method for a bot
  * @returns JSON file of the bot
  */
-router.get('/', function(req, res, next) {
+router.get('/:botName', function(req, res, next) {
     //must render a JSON with the bot
-    //TODO: url with bot name, JSON
+    //TODO: JSON
 });
 
 /**@function POST Bot
  * @summary Define the creation of a brain
  * @returns
  */
-router.post('/', function(req, res, next) {
+router.post('/:botName-:brainName', function(req, res, next) {
     //takes a JSON to create a bot ? or create a json from parameters in the url ?
     //TODO: url with bot characteristics, JSON
 });
@@ -26,18 +26,18 @@ router.post('/', function(req, res, next) {
  * @summary Define the modification of a brain
  * @returns
  */
-router.put('/', function(req, res, next) {
+router.put('/:botName-:brainName', function(req, res, next) {
     //must modify a parameter (in the url) of the brain
-    //TODO: url with bot name, JSON
+    //TODO: JSON
 });
 
 /**@function DELETE Bot
  * @summary Define the deletion of a brain
  * @returns
  */
-router.delete('/', function(req, res, next) {
+router.delete('/:botName', function(req, res, next) {
     //must delete the brain with this name
-    //TODO: url with bot name, JSON
+    //TODO: JSON
 });
 
 module.exports = router;
