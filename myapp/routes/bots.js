3ee3a872a86208ec84a941569fc20ff8423564ec
Lily <lil.wad.44@gmail.com>
2019-06-07 11:47:04 +0200

    Suite de commit précédent

--- myapp/routes/bots.js
+++ myapp/routes/bots.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
-var Rivescript = require('Rivescript');
 var router = express.Router();
 
 /**@function GET Bot
